85beb86bda86a0529c6867e553676c35659d2e0d
“Alexander <alex@anbdesign.com>
2023-07-13 16:02:29 -0700

    Dockerized the app

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 6001
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
