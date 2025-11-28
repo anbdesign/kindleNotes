da323caa755ea533f50895d134c99f7eba1ef380
“Alexander <alex@anbdesign.com>
2025-11-28 14:41:02 -0800

    deleted commented out code

--- dist/server.js
+++ dist/server.js
@@ -3,19 +3,17 @@ const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 6001;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 app.use(express.static('dist/app'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false,
 }));
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
 app.listen(port, () => {
     console.log('listening on *:' + port);
 });
+// NOT SURE IF THIS IS NEEDED, BUT SAW IN TUTORIAL
 // app.get('/todo', function (req, res) {
 //   // 👇️ if your HTML file is in the root directory (next to package.json)
 //   res.sendFile(__dirname + '/index.html');
@@ -38,20 +36,4 @@ app.post('/', (request, result) => {
   <a href='/'>Send another?</a>
   `);
 });
-// POST method route
-// app.get('/todo/:todoText', (req, res) => {
-//   console.log(req.params.todoText);
-// const dateString = new Date().toString()
-// console.log(`Form Submitted: ${input}`);
-// const message = {
-//     to: process.env.THINGS_EMAIL,
-//     from: process.env.FROM_EMAIL,
-//     subject: input,
-//     text: `Kindle Note from ${dateString}`
-// }
-// sgMail.send(message)
-// .then((response)=> console.log('email sent...'))
-// .catch((error)=> {console.log(error.message)})
-//   res.send('POST request to the homepage')
-// })
 //# sourceMappingURL=server.js.map
